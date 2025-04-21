c2cbb8d45d7a9c93d9533f402c5a9aa015e59d58
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-21 23:24:24 +0000

    Loads menu items from array (also added some code for categories, oops

--- src/menu.js
+++ src/menu.js
@@ -5,31 +5,66 @@ export function createMenu(){
     main.id = "main-container";
     
 
-    const homeHeaderNames = {
-        About: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore voluptatem aut officiis saepe nesciunt, corporis non eum quo optio tenetur esse sapiente modi laboriosam facere deserunt ducimus reprehenderit deleniti quos!",
-        Hours: "",
-        Location: "This place"
-    };
+    const menuItems = [];
+
+    class MenuItem{
+        constructor(name, category, description, calories, cost){
+            this.category = category;
+            this.name = name;
+            this.description = description;
+            this.calories = calories;
+            this.cost = cost;
+        }
+    }
+
+    function addMenuItem(name, category, description, calories, cost){
+        menuItems.push(new MenuItem(name, category, description, calories, cost));
+    }
+
+    addMenuItem("1st Item", "Chungus", "this thing", "2100 Calories", 5.99);
+    addMenuItem("2st Item", "Thungus", "this thing", "2100 Calories", 5.99);
+    addMenuItem("3st Item", "Thungus", "this thing", "2100 Calories", 5.99);
+    addMenuItem("4st Item", "Chungus", "this thing", "2100 Calories", 5.99);
+    
 
     const title = document.createElement("div");
     title.textContent = "Waffles Waffles to Go";
     title.id = "Title";
-    title.classList = "home-header";
+    title.classList = "menu header";
     main.appendChild(title);
 
-    for (const key in homeHeaderNames){
+     
+
+    for (const key in menuItems){
+        
+        const currentItem = menuItems[key];
+        var category = document.querySelector("#"+currentItem.category);
+        if (currentItem.category === null) {
+            const category = document.createElement("div");
+            category.id = currentItem.category;
+        }
+
         const header = document.createElement("div");
-        header.id = key;
-        header.classList = "home-header";
+        console.log(currentItem.name);
+        header.id = currentItem.name;
+        header.classList = "menu header";
 
-        const headerTitle = document.createElement("h2");
-        const headerAbout = document.createElement("p");
+        const headerName = document.createElement("h2");
+        const headerPrice = document.createElement("p");
+        const headerDescription = document.createElement("p");
+        const headerCalories = document.createElement("p");
 
-        headerTitle.textContent = key;
-        headerAbout.textContent = homeHeaderNames[key];
+        headerName.textContent = currentItem.name;
+        headerDescription.textContent = currentItem.description;
+        headerPrice.textContent = currentItem.cost;
+        console.log(currentItem.cost);
+        headerCalories.textContent = currentItem.calories;
+        
 
-        header.appendChild(headerTitle);
-        header.appendChild(headerAbout);
+        header.appendChild(headerName);
+        header.appendChild(headerPrice);
+        header.appendChild(headerDescription);
+        header.appendChild(headerCalories);
 
         main.appendChild(header);
     };
