a07cc3f8f398fea19179f845471f5a33f7415ddd
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-21 17:37:00 +0000

    created headers for homepage using DOM manipulation

--- src/home-page.js
+++ src/home-page.js
@@ -2,7 +2,15 @@ export function createHomepage(){
     const content = document.getElementById("content");
     const main = document.createElement("div");
     main.id = "main-container";
+    
+
+    var homeHeaderNames = ["Title","About", "Hours","Location"];
+    
+    homeHeaderNames.forEach(name =>{
+        const header = document.createElement("div");
+        header.id = name;
+        main.appendChild(header);
+    });
+
     content.appendChild(main);
-    
-    
 };
