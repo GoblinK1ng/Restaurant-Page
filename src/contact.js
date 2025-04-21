a242a73ae2246f72fedf4fbf9b2b5070e0c39a39
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-21 22:45:32 +0000

    Added modules for Contact and Menu sections (filled with homepage code for now) and linked to index.js

---
+++ src/contact.js
@@ -0,0 +1,52 @@
+export function createContact(){
+    const content = document.getElementById("content");
+    content.innerHTML = "";
+    const main = document.createElement("div");
+    main.id = "main-container";
+    
+
+    const homeHeaderNames = {
+        About: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore voluptatem aut officiis saepe nesciunt, corporis non eum quo optio tenetur esse sapiente modi laboriosam facere deserunt ducimus reprehenderit deleniti quos!",
+        Hours: "",
+        Location: "This place"
+    };
+
+    const title = document.createElement("div");
+    title.textContent = "Waffles Waffles to Go";
+    title.id = "Title";
+    title.classList = "home-header";
+    main.appendChild(title);
+
+    for (const key in homeHeaderNames){
+        const header = document.createElement("div");
+        header.id = key;
+        header.classList = "home-header";
+
+        const headerTitle = document.createElement("h2");
+        const headerAbout = document.createElement("p");
+
+        headerTitle.textContent = key;
+        headerAbout.textContent = homeHeaderNames[key];
+
+        header.appendChild(headerTitle);
+        header.appendChild(headerAbout);
+
+        main.appendChild(header);
+    };
+
+    
+
+    content.appendChild(main);
+
+    
+
+    const about = document.getElementById("About");
+    
+
+
+    const hours = document.getElementById("Hours");
+    
+
+    const location = document.getElementById("Location");
+    
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 console.log("THis exists");
 
 import { createHomepage } from "./home-page";
+import { createContact } from "./contact";
+import { createMenu } from "./menu";
 import "./style.css";
 
 const homeButton = document.getElementById("Home");
