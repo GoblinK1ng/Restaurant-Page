d1289d620fe7c8d0e44a77d1ae05d661cca94c87
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-21 17:53:39 +0000

    Added header elements with ids and classes with dom manipulation, added styling through css

--- src/home-page.js
+++ src/home-page.js
@@ -5,12 +5,27 @@ export function createHomepage(){
     
 
     var homeHeaderNames = ["Title","About", "Hours","Location"];
-    
+
     homeHeaderNames.forEach(name =>{
         const header = document.createElement("div");
         header.id = name;
+        header.classList = "home-header";
         main.appendChild(header);
     });
 
+    
+
     content.appendChild(main);
+
+    const title = document.getElementById("Title");
+    title.textContent = "Waffles Waffles to Go";
+
+    const abouts = document.getElementById("About");
+    abouts.textContent = "Waffles Waffles to Go";
+
+    const hours = document.getElementById("Hours");
+    hours.textContent = "Waffles Waffles to Go";
+
+    const location = document.getElementById("Location");
+    location.textContent = "Waffles Waffles to Go";
 };
